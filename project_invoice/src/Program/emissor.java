2b87a7ae172466fe6f950178ea280b0c23f3c301
recepcao <souza.rocha2000@gmail.com>
2024-08-15 16:34:20 -0300

    FIRST COMMIT - Creation of classes and methods

---
+++ project_invoice/src/Program/emissor.java
@@ -0,0 +1,12 @@
+package Program;
+
+public class emissor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int teste = 123;
+		
+		System.out.print(teste);
+	}
+
+}
