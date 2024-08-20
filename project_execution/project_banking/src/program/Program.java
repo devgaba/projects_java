9c29543a2273cdc5c2d975322fafca60c0a22153
recepcao <souza.rocha2000@gmail.com>
2024-08-20 16:21:15 -0300

    In Execution | Project - BANK OF GOLD === FIRST COMMIT

---
+++ project_execution/project_banking/src/program/Program.java
@@ -0,0 +1,16 @@
+package program;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		
+		
+		input.close();
+	}
+
+}
