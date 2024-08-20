89e990d5e84630c4a3e9c7e70cdbca2525bad064
recepcao <souza.rocha2000@gmail.com>
2024-08-20 15:25:21 -0300

    Temporary | Project - DSR === FIRST COMMIT

---
+++ project_dsr/project_dsr/src/temporary/Program.java
@@ -0,0 +1,34 @@
+package temporary;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.println("===========ÍNICIO============");
+		System.out.print(" Digite o valor bruto: ");
+		double comissao = input.nextDouble();
+		
+		System.out.printf("DSR ------------> R$ %.2f%n", Calculator(comissao));
+		System.out.printf("COMISSÃO -------> R$ %.2f%n", sub(comissao));
+		System.out.printf("VALOR BRUTO ----> R$ %.2f%n", Calculator(comissao));
+		
+		System.out.println("=============================");
+		
+		input.close();
+	}
+	
+	public static double Calculator(double comissao) {
+		return (12.903* comissao)/ 100;
+	}
+	public static double sub(double comissao) {
+		return comissao - Calculator(comissao);
+	}
+
+}
