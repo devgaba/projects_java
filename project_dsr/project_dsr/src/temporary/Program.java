d8be2e7862b621c7ca5e0a852c16aefd4c88c1ea
Gabriel Henrique <155110895+devgaba@users.noreply.github.com>
2024-10-02 16:06:58 -0300

    UPDATE PROJECT DSR ---> DSR

--- project_dsr/project_dsr/src/temporary/Program.java
+++ project_dsr/project_dsr/src/temporary/Program.java
@@ -25,7 +25,7 @@ public class Program {
 	}
 	
 	public static double Calculator(double comissao) {
-		return (21.7749* comissao)/ 100;
+		return (20.00027* comissao)/ 100;
 	}
 	public static double sub(double comissao) {
 		return comissao - Calculator(comissao);
